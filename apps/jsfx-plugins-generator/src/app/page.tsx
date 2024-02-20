b6726e780fd07e5473b10f70ce278491b3568f22
mthines <mads.thines@dash0.com>
2024-02-20 09:34:42 +0100

    Remove Google Analytics import from page.tsx

--- apps/jsfx-plugins-generator/src/app/page.tsx
+++ apps/jsfx-plugins-generator/src/app/page.tsx
@@ -1,5 +1,3 @@
-import { GoogleAnalytics } from '@next/third-parties/google';
-
 import { ClientProviders } from '@jsfx-plugins-generator/components/providers';
 
 import { SliderForm } from '@jsfx-plugins-generator/features/jsfx/plugin';
@@ -7,7 +5,6 @@ import { SliderForm } from '@jsfx-plugins-generator/features/jsfx/plugin';
 export default function Page() {
   return (
     <div>
-      <GoogleAnalytics gaId="G-2WRDHYCVCQ" />
       <ClientProviders>
         <SliderForm />
       </ClientProviders>
