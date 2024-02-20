c7c99088f6c13c6917eea96c94d0053424fbfa9b
mthines <mads.thines@dash0.com>
2024-02-20 10:27:17 +0100

    Add metadata to page component

--- apps/jsfx-plugins-generator/src/app/page.tsx
+++ apps/jsfx-plugins-generator/src/app/page.tsx
@@ -1,6 +1,32 @@
+import { Metadata } from 'next';
+
 import { ClientProviders } from '@jsfx-plugins-generator/components/providers';
 
 import { SliderForm } from '@jsfx-plugins-generator/features/jsfx/plugin';
+
+export const metadata: Metadata = {
+  title: 'JSFX Plugin Generator for Reaper DAW (Cockos)',
+  description:
+    'This tool helps you generator JSFX plugins for Reaper DAW (Cockos)™. Create and adjust the sliders to your match your needs, and then download the plugin. Place the downloaded plugin in the Reaper source directory and then refresh the effects list.',
+  keywords: [
+    'jsfx',
+    'no code',
+    'reaper',
+    'cockos',
+    'daw',
+    'plugin',
+    'vst',
+    'generator',
+    'slider',
+    'cc',
+    'github',
+    'open source',
+    'free',
+    'download',
+    'presets',
+    'midi',
+  ],
+};
 
 export default function Page() {
   return (
